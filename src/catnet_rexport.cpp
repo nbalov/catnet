87e1fab6a990d16550468e3e526ab4836203640b
Nikolay Balov <nhbalov@gmail.com>
2020-02-25 20:01:17 -0600

    modified:   src/catnet_rexport.cpp

--- src/catnet_rexport.cpp
+++ src/catnet_rexport.cpp
@@ -25,11 +25,7 @@
  */
 
 /* 
-<<<<<<< HEAD
  * version 1.15.6  25feb2020
-=======
- * version 1.15.6  20may2019
->>>>>>> fc5dfb4a994ceb6fa8743220098f9d43937f60fa
  */
 
 #include "utils.h"
@@ -276,17 +272,10 @@ SEXP showCatnet(SEXP cnet)
 }
 
 SEXP catnetOptimalNetsForOrder(SEXP rSamples, SEXP rPerturbations, 
-<<<<<<< HEAD
 	SEXP rMaxParents, SEXP rParentSizes, SEXP rMaxComplexity, 
 	SEXP rOrder, SEXP rNodeCats, 
 	SEXP rParentsPool, SEXP rFixedParentsPool, SEXP rMatEdgeLiks, 
 	SEXP rUseCache, SEXP rEcho) {
-=======
-                              SEXP rMaxParents, SEXP rParentSizes, 
-							  SEXP rMaxComplexity, SEXP rOrder, SEXP rNodeCats, 
-                              SEXP rParentsPool, SEXP rFixedParentsPool, 
-							  SEXP rMatEdgeLiks, SEXP rUseCache, SEXP rEcho) {
->>>>>>> fc5dfb4a994ceb6fa8743220098f9d43937f60fa
 
 	//if(!isMatrix(rSamples))
 	//	error("Data is not a matrix");
